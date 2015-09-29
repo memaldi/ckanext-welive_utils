2a3e714f0509aeeca8094c0573eff48779c78499
Mikel Emaldi <m.emaldi@gmail.com>
2015-09-29 10:42:00 +0200

    Purging datasets when deleting

--- ckanext/welive_utils/plugin.py
+++ ckanext/welive_utils/plugin.py
@@ -1,9 +1,24 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.logic.action import delete
+from ckan.model.package import Package
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def package_delete(context, data_dict):
+    model = context['model']
+    delete.package_delete(context, data_dict)
+    package = Package.get(data_dict['id'])
+    package.purge()
+    model.repo.commit_and_remove()
+    return None
 
 
 class Welive_UtilsPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IActions)
 
     # IConfigurer
 
@@ -11,3 +26,8 @@ class Welive_UtilsPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'welive_utils')
+
+    # IActions
+
+    def get_actions(self):
+        return {'package_delete': package_delete}
