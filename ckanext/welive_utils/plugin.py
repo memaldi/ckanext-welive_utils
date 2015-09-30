e9cb7f996ff3e161c725d203547cd7a3597c3709
Mikel Emaldi <m.emaldi@gmail.com>
2015-09-30 09:44:54 +0200

    Modified the resource_update action for avoiding asking the url when not changed

--- ckanext/welive_utils/plugin.py
+++ ckanext/welive_utils/plugin.py
@@ -1,7 +1,9 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckan.logic.action import delete
+from ckan.logic.action import update
 from ckan.model.package import Package
+from ckan.model.resource import Resource
 import logging
 
 log = logging.getLogger(__name__)
@@ -14,6 +16,17 @@ def package_delete(context, data_dict):
     package.purge()
     model.repo.commit_and_remove()
     return None
+
+
+def resource_update(context, data_dict):
+    resource = Resource.get(data_dict['id'])
+    if resource is not None and 'url' not in data_dict:
+        url = resource.url
+        data_dict['url'] = url
+        log.debug(url)
+    log.debug(data_dict)
+    package_dict = update.resource_update(context, data_dict)
+    return package_dict
 
 
 class Welive_UtilsPlugin(plugins.SingletonPlugin):
@@ -30,4 +43,5 @@ class Welive_UtilsPlugin(plugins.SingletonPlugin):
     # IActions
 
     def get_actions(self):
-        return {'package_delete': package_delete}
+        return {'package_delete': package_delete,
+                'resource_update': resource_update}
