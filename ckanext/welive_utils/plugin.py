bf9c29d5b78eaae45afbd5cfd720487cf9689d04
Mikel Emaldi <m.emaldi@gmail.com>
2015-11-09 09:52:41 +0100

    Added language field to Datasets

--- ckanext/welive_utils/plugin.py
+++ ckanext/welive_utils/plugin.py
@@ -47,6 +47,7 @@ def resource_create(context, data_dict):
 class Welive_UtilsPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IActions)
+    plugins.implements(plugins.IDatasetForm)
 
     # IConfigurer
 
@@ -62,3 +63,37 @@ class Welive_UtilsPlugin(plugins.SingletonPlugin):
                 'resource_update': resource_update,
                 'resource_create': resource_create
                 }
+
+    # IDatasetForm
+
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return False
+
+    def package_types(self):
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        return []
+
+    def create_package_schema(self):
+        schema = super(Welive_UtilsPlugin, self).create_package_schema()
+        schema.update({
+            'language': [toolkit.get_converter('convert_to_extras')]
+        })
+        return schema
+
+    def update_package_schema(self):
+        schema = super(Welive_UtilsPlugin, self).update_package_schema()
+        # our custom field
+        schema.update({
+            'language': [toolkit.get_converter('convert_to_extras')]
+        })
+        return schema
+
+        def show_package_schema(self):
+            schema = super(Welive_UtilsPlugin, self).show_package_schema()
+            schema.update({
+                'language': [toolkit.get_converter('convert_from_extras')]
+            })
+            return schema
