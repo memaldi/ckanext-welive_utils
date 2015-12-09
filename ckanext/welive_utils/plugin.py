3d12a3364a821f3bd76558deb53e689b9d3b449a
Mikel Emaldi <m.emaldi@gmail.com>
2015-12-09 18:57:59 +0100

    Fixing bugs

--- ckanext/welive_utils/plugin.py
+++ ckanext/welive_utils/plugin.py
@@ -72,11 +72,11 @@ def package_show(context, data_dict):
 def package_create(context, data_dict):
     if type(data_dict) is dict:
         mapped_resources = []
-        if 'resource' in data_dict:
+        if 'resources' in data_dict:
             for resource in data_dict['resources']:
                 mapped_resource = generate_mapping(context, resource)
                 mapped_resources.append(mapped_resource)
-        data_dict['resources'] = mapped_resources
+            data_dict['resources'] = mapped_resources
 
     log.debug(data_dict)
 
